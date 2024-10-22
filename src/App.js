e4a331b2833dd76a08d5f97d265bf21a69adfab4
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2024-10-22 04:17:52 +0300

    Structure componets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import Home from './Pages/Home'; // Adjust the path if needed
+import Header from './Components/Header'; // Adjust the path if needed
 
 function App() {
   return (
-    <>
-      <h1 className="text-danger">E-commerce Website in JS</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
