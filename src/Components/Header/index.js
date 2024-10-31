8cff4f94df8cc48ff904be0116944d1e600baa42
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2024-10-31 12:25:32 +0300

    design home page

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,6 +1,16 @@
 const Header = () => (
   <>
-    <h1>Header</h1>
+    <div className="headerWrapper">
+      <div className="top-strip bg-blue">
+        <div className="container">
+          <p className="mb-0 mt-0 text-center">
+            Due to the
+            <b> COVID 19 </b>
+            epidemic, orders may be processed with a slight delay
+          </p>
+        </div>
+      </div>
+    </div>
   </>
 );
 
