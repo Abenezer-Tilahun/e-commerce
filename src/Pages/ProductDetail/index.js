161cfa2897809a524842ffa43ea4fdf354bbb012
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2024-11-22 14:22:00 +0300

    Design Product Detail Page

---
+++ src/Pages/ProductDetail/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-cycle */
+import React from 'react';
+
+const ProductDetail = () => (
+  <div>
+    <h1>Product Details</h1>
+    <p>Description of the product...</p>
+  </div>
+);
+
+export default ProductDetail;
